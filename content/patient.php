52c4036d017c94bbae35dd96c9d890e40258ea9b
ma7ma7 <mahmoudamane5@gmail.com>
2018-06-24 20:29:01 +0100

    deplace Patient form login to another separate file

---
+++ content/patient.php
@@ -0,0 +1,34 @@
+<div class="patient">
+    <form action="" method="post">
+      <label for="nom">Nom <span>*</span> :</label>
+      <input type="text" name="nom" id="nom" placeholder="Votre Nom"/></br>
+      
+      <label for="prenom">Prenom <span>*</span> :</label>
+      <input type="text" name="prenom" id="prenom" placeholder="Votre Prenom"/></br>
+
+      <label for="birthday">Date de naissance <span>*</span> :</label>
+      <input type="date" name="birthday" id="birthday"/></br>
+
+      <label for="">Sexe <span>*</span> :</label>
+      <select name="sexe" id="sexe">
+        <option value="masculin">Masculin</option>
+        <option value="Feminin">Feminin</option>
+      </select></br>
+
+      <label for="email">Email <span>*</span> :</label>
+      <input type="email" name="email" id="email" placeholder="Votre adresse E-mail"/></br>
+
+      <label for="password"> Mot de passe <span>*</span> :</label>
+      <input type="password" name="password" id="password" placeholder="Votre Mot de passe"/></br>
+
+      <label for="confirm_password"> Confirmation de Mot de passe <span>*</span> :</label>
+      <input type="password" name="confirm_password" id="confirm_password" placeholder="Confirmer votre Mot de passe"/></br>
+
+      <input type="checkbox" name="confirmation" id="confirmation"/>
+      <label id="confirmation" class="terme_butt"><a href="#">J'ai Lue et j'accept les conditions ...</a></label>
+
+      <input type="submit" value="Valider"/></br>
+
+      <label> <span>*</span> Les champs obligatoires</label>
+    </form>
+  </div>
